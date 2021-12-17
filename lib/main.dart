3b7814d6d16e13fa47ad8e288d98546369008571
Roman Senin <romansenin15@gmail.com>
2021-12-16 16:46:52 -0800

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:chat_app/screens/chat_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'FlutterChat',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: ChatScreen(),
+    );
+  }
+}
