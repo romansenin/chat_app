dd890514bb1a1271e3ecded3b9a01438a36dc661
Roman Senin <romansenin15@gmail.com>
2021-12-21 16:13:03 -0800

    listen to chat messages

---
+++ lib/widgets/chat/new_message.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class NewMessage extends StatefulWidget {
+  @override
+  _NewMessageState createState() => _NewMessageState();
+}
+
+class _NewMessageState extends State<NewMessage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 8),
+      padding: EdgeInsets.all(8),
+      child: Row(
+        children: [
+          Expanded(
+            child: TextField(
+              decoration: InputDecoration(labelText: 'Send a message...'),
+              onChanged: (value) {},
+            ),
+          ),
+          IconButton(
+              color: Theme.of(context).primaryColor,
+              icon: Icon(Icons.send),
+              onPressed: () {}),
+        ],
+      ),
+    );
+  }
+}
